c65cdfc5b4f315be7cd094c2563d1df17ef632c4
Diogo Ewerton <diogoesn1212@gmail.com>
2025-11-30 19:48:37 -0300

    Update VendaControle.java

--- untitled/src/VendaControle.java
+++ untitled/src/VendaControle.java
@@ -1,18 +1,35 @@
-import Cliente.java
-import ClienteService.java
-import java.until.list
+import java.util.List;
 
 public class VendaControle {
 
-  private final VendaService service = new VendaService();
-  private final ProdutoService produtoService = new ProdutoService();
-  private final NotaFiscalService notaService = new NotaFiscalService();
+    private final VendaService vendaService;
+    private final ProdutoService produtoService;
+    private final ClienteService clienteService;
+    private final NotaFiscalService notaService;
 
- public NotaFiscal realizarVenda(Venda venda) {
-    vendaService.registrar(venda);
-    produtoService.atualizarEstoque(venda.getProduto(), venda.getQuantidade());
-    return notaService.emitirNota(venda);
-  }
 
-  public double exibirTotalDeVendas() { return vendaService.totalVendas(); }
+    public VendaControle(ProdutoService produtoService, ClienteService clienteService,
+                         VendaService vendaService, NotaFiscalService notaService) {
+        this.produtoService = produtoService;
+        this.clienteService = clienteService;
+        this.vendaService = vendaService;
+        this.notaService = notaService;
+    }
+
+
+    public NotaFiscal realizarVenda(Venda venda) {
+        vendaService.registrar(venda);
+        produtoService.atualizarEstoque(venda.getProduto(), venda.getQuantidade());
+        return notaService.emitirNota(venda);
+    }
+
+
+    public double exibirTotalDeVendas() {
+        return vendaService.totalVendas();
+    }
+
+
+    public List<Venda> listarVendas() {
+        return vendaService.listarVendas();
+    }
 }
